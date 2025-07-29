b0bc3c87dea51b80612a88289da92d58c9328f0d
Yash Shaw <yshaw2002@gmail.com>
2025-07-30 02:07:12 +0530

    file upload for vocerimg via multer added

--- routes/blog.js
+++ routes/blog.js
@@ -1,10 +1,33 @@
-const { Router } = require("express")
-const router = Router()
+const { Router } = require("express");
+const router = Router();
+const multer = require("multer");
+const path = require("path");
 
-router.get("/add-new",(req,res)=>{
-    return res.render("addBlog",{
-        user: res.user,
-    })
-})
+router.get("/add-new", (req, res) => {
+  return res.render("addBlog", {
+    user: req.user,
+  });
+});
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve("./public/uploads"));
+    },
+    filename: function (req, file, cb) {
+      const filename = `${Date.now()}-${file.originalname}`;
+      cb(null, filename);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
+
+router.post("/", upload.single('coverImage'),(req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+  
+  return res.redirect("/");
+});
+
+
 
 module.exports = router;
