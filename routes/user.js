067efaecb1d17e8023ff4d9517c821f8722b42b0
Yash Shaw <yshaw2002@gmail.com>
2025-07-30 00:50:39 +0530

    created blog schema

--- routes/user.js
+++ routes/user.js
@@ -10,6 +10,10 @@ router.get("/signup", (req, res) => {
   res.render("signup");
 });
 
+router.get("/logout",(req,res)=>{
+  res.clearCookie("token").redirect("/")
+})
+
 router.post("/signup", async (req, res) => {
   const { fullName, email, password } = req.body;
   await User.create({
