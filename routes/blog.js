5b721a245f609f71dbdeeca9245c1256d7971eee
Yash Shaw <yshaw2002@gmail.com>
2025-08-01 02:18:54 +0530

    complted Blogify

--- routes/blog.js
+++ routes/blog.js
@@ -41,14 +41,18 @@ router.post("/", upload.single("coverImage"), async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   const blog = await Blog.findById(req.params.id).populate("createdBy");
+  const comments = await Comment.find({blogId: req.params.id}).populate("createdBy")
+  console.log("comment",comments);
+  
   return res.render("blog", {
     user: req.user,
     blog,
+    comments,
   });
 });
 
 router.post("/comment/:blogId", async (req, res) => {
-  const comment = await Comment.create({
+  await Comment.create({
     content: req.body.content,
     blogId: req.params.blogId,
     createdBy: req.user._id,
